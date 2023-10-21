4adf72d65e057fe71f0667163a525ee37de0a2dc
Denis-Baranov <denizzka@tut.by>
2023-10-22 02:35:55 +0300

    Initial commit

---
+++ src/main/java/by/itacademy/dbaranov/Calc.java
@@ -0,0 +1,23 @@
+package by.itacademy.dbaranov;
+
+public class Calc {
+    public int sum(int a, int b) {
+        return a + b;
+
+    }
+
+    public int substraction(int a, int b) {
+        return a - b;
+
+    }
+
+    public double division(double a, double b) {
+        return a / b;
+
+    }
+
+    public int multiplication(int a, int b) {
+        return a * b;
+
+    }
+}
